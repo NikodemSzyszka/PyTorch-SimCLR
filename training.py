1161bb03b8be1eb1a96c67ec10c59ae4f0574246
Nikodem Szyszka <32900768+NikodemSzyszka@users.noreply.github.com>
2023-03-13 14:57:19 +0100

    loss function

--- training.py
+++ training.py
@@ -8,10 +8,19 @@ class TrainingModule():
         self.args = kwargs['args']
         self.model = kwargs['model'].to(self.args.device)
         self.optimizer = kwargs['optimizer']
+        self.criterion = nn.CrossEntropyLoss().to(self.args.device)
         
+    def loss(self, representations):
+        similarity = nn.functional.cosine_similarity(representations.unsqueeze(1), representations, dim = -1)
+        return self.criterion(similarity, torch.zeros(similarity.shape[0], dtype = torch.long).to(self.args.device))
 
     def train(self, train_loader):
         for epoch_counter in range(self.args.epochs):
             for i, (images, _) in enumerate(tqdm(train_loader, desc = f"Epoch: {epoch_counter}")):
-                pass
+                images = torch.cat(images, dim=0).to(self.args.device)
+                representations = self.model(images)
+                loss = self.loss(representations)
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
 
