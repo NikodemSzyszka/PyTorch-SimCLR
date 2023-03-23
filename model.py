5705b7a0533084a743d0f5a65465a1fe0421b030
Nikodem Szyszka <32900768+NikodemSzyszka@users.noreply.github.com>
2023-03-23 02:40:56 +0100

    cifar10 model changes

--- model.py
+++ model.py
@@ -1,16 +1,19 @@
 import torch.nn as nn
 import torchvision.models as models
+from torch.cuda.amp import autocast
 
 class SimCLR(nn.Module):
 
-    def __init__(self):
+    def __init__(self, dataset_name):
         super(SimCLR, self).__init__()
-        self.resnet = models.resnet18(pretrained=False, num_classes = 2048)
-        self.resnet.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.resnet = models.resnet50(pretrained=False, num_classes = 2048)
+        if dataset_name != 'cifar10':
+            # mnist and fmnist
+            self.resnet.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
         self.resnet.fc = nn.Identity()
         self.mlp = nn.Sequential(nn.Linear(2048, 2048), nn.ReLU(), nn.Linear(2048, 128))
-    
+        
     def forward(self, x):
-        x = self.resnet(x)
+        z = self.resnet(x)
         return self.mlp(z)
 
